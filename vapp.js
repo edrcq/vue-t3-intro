a587a73b59b622c924715f4113ad1d1561ca79c5
Ed <erucquoy@gmail.com>
2021-01-18 12:27:13 +0100

    updated

--- vapp.js
+++ vapp.js
@@ -1,3 +1,4 @@
+
 Vue.component('list-item', {
     template: `<li>{{ text }}</li>`,
 
@@ -5,7 +6,10 @@ Vue.component('list-item', {
         text: { type: String, default: 'test' }
     }
 })
-Vue.directive('focus', {
+
+
+// Ceci est une directive custom
+Vue.directive('focusa', {
     // Quand l'élément lié est inséré dans le DOM...
     inserted: function (el) {
       // L'élément prend le focus
@@ -14,20 +18,72 @@ Vue.directive('focus', {
 })
 
 const vapp = new Vue({
-    el: '#todo-app',
+
+    components: [
+
+    ],
 
     data() { return {
         newTodo: '',
-        title: 'Todo List',
-        todos: ['Todo 1', 'Todo 2']
+        title: '<h1>Todo List<h1>',
+        todos: [
+            { text: 'Todo 1', done: false },
+            { text: 'Todo 2', done: false },
+        ]
     }},
 
     computed: {
         maxLenInputStyle() {
             return this.newTodo.length > 8 ? 'color: red;' : 'color: green;'
         }
-    }
+    },
+
+    methods: {
+        addItem() {
+            this.todos.push({
+                text: this.newTodo,
+                done: false
+            })
+            this.newTodo = ''
+        }
+    },
+
+    /* Cycles de vie */
+    beforeCreate() {
+        console.log('before create')
+        fetch(url).then(r => r.json()).then(jsonData => this.todos = jsonData)
+    },
+    created() {
+        console.log('created')
+    },
+
+    beforeMount() {
+        console.log('before mount')
+    },
+    mounted() {
+        console.log('mounted')
+    },
+
+    beforeUpdate() {
+        console.log('before update', this.title)
+    },
+    updated() {
+        console.log('updated', this.title)
+    },
+
+    beforeDestroy() {}
 
 
 })
+vapp.$mount('#todo-app')
+
+
+
+setTimeout(() => {
+    vapp.title = 'Titre ToDo List'
+}, 3000)
+
+
+
+
 
